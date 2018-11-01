1fcf7120aceeb49dcc9801305c3b9880f40ded14
jannikschwarz <44576107+jannikschwarz@users.noreply.github.com>
2018-11-01 14:49:15 +0100

    Add files via upload

--- Simulator.java
+++ Simulator.java
@@ -15,108 +15,56 @@ public class Simulator {
 	TOTAL_SIMULATION_TIME = 7,
 	START_PROGRAM = 8,
 	EXIT_PROGRAM = 0;
-	private static EventQueue eventQueue = new EventQueue();
-	private static Population population = new Population(0.003);
+	public static EventQueue eventQueue = new EventQueue();
+	public static Population population = new Population(0.003);
 	private static int userInput;
-
-	private static int initialPopulationSize = 20;
-
+	private static Random random = new Random();
+	public static int n = 42;
+	public static int M = 20;
+	public static int nMax = 50;
+	public static int R = 10;
+	public static double D = 0.002;
 	/*
 	* Initializes scanner which allows the user to interact with the program.
 	*/
-
 	private static Scanner sc = new Scanner(System.in);
 
 
 
-	private static boolean run = true;
-
 	public static void main(String[] args) {
 
-		/*
-		* Initializes EventQueue and Population instances.
-		* CityGenerator has a static method called generate()
-		* and it is therefore not neccesary to initialize CityGenerator as an instance.
-		*/
-
-
-
-		/*
-		* Prints out the name of the cities (test).
-		*/
-
-		//for(int i = 0; i < CityGenerator.generate().length; i++){
-		//System.out.println(CityGenerator.generate()[i].name());
-		// }
-
-		//population.add(individual = new Individual());
-		//System.out.println(population.size());
-		initialPopulation(initialPopulationSize);
-		cityGenerator();
-		printBestPath();
-
-		// Note to myself. This might generate the best designated path to the cities of the population
-
-
-		do {
-			//textMenu();
-			//inputMenu();
-			//!run will only make the program to run once.
-		}while(!run);
-	}
-
-	/*
-	* Prints out a text menu for the user.
-	*/
-
-	private static void textMenu(){
-		System.out.println("__________________________________");
-		System.out.println("");
-		System.out.println("1. Initial size of the population");
-		System.out.println("2. Maximum population size");
-		System.out.println("3. Mutation interval");
-		System.out.println("4. Reproduction interval");
-		System.out.println("5. Death interval");
-		System.out.println("6. Comfort normalization");
-		System.out.println("7. Total simulation time");
-		System.out.println("__________________________________");
-		System.out.println("");
-		System.out.println("8. Start program");
-		System.out.println("0. Quit program");
-		System.out.println("__________________________________");
-	}
-
-	/*
-	* Allows the user to interact with the program.
-	* The range of the input is 1 - 8 and 0.
-	*/
-
-	private static void inputMenu(){
-		System.out.println("");
-		System.out.println("Enter here: ");
-		userInput = sc.nextInt();
-
-	}
-
-	private static void initialPopulation(int size){
-		int i = 0;
-		while(i < size) {
-			population.add(new Individual(CityGenerator.generate()));
-			System.out.println(population.size());
-			i++;
+		for(int i = 0; i < n; i++){
+			individuals(eventQueue, population);
 		}
-	}
-	private static void maxPopulation(int population){
-	}
-
-	private static void cityGenerator(){
-		for(int i = 0; i < CityGenerator.generate().length; i++)
-			System.out.println(CityGenerator.generate()[i].name() + " " + CityGenerator.generate()[i].x() + " " + CityGenerator.generate()[i].y());
+		System.out.println(population.size());
+		System.out.println(mut);
+		System.out.println(rep);
+		System.out.println(dead);
 	}
 
-	private static void printBestPath(){
-		for(int i = 0; i < CityGenerator.generate().length; i++)
-			System.out.println(population.bestPath()[i].name());
-	}
+	public static void individuals(EventQueue eventQueue,Population population){
+		//Creates a new individual and places him in population
+		City[] city = CityGenerator.generate();
+		Individual individual = new Individual(city);
+		population.add(individual);
 
+		//Defines the probability of events
+		final double fit = 1 - Math.log(population.fitness(individual));
+		final double mut = RandomUtils.getRandomTime(fit * M);
+		final double rep = RandomUtils.getRandomTime(fit * (n / nMax) * R);
+		final double dead = RandomUtils.getRandomTime(fit * D);
+
+		//Makes an object type event.
+		//Event(char type, double time, Individual individual) - To remember parameters
+		Event eventMutation = new Event('m', mut, individual);
+		Event eventReproduction = new Event('R', rep, individual);
+		Event eventDeath = new Event('D', dead, individual);
+
+
+		//adds events to eventQueue
+		eventQueue.add(eventMutation);
+		eventQueue.add(eventReproduction);
+		eventQueue.add(eventDeath);
+		System.out.println(mut + rep + dead);
+	}
 }
