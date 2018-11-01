87a3e556c6789ff7ac5c3205183b943be2c330b0
jannikschwarz <44576107+jannikschwarz@users.noreply.github.com>
2018-11-01 15:55:19 +0100

    Add files via upload

--- Simulator.java
+++ Simulator.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 public class Simulator {
-
 	/*
 	* Initializes final variables for the user to choose, and the userInput variable.
 	*/
@@ -31,8 +30,8 @@ public class Simulator {
 	* Initialises the default values.
 	*/
 
-	private static int I_POP_SIZE = 20, 
-	MAX_POP_SIZE = 200, 
+	private static int I_POP_SIZE = 20,
+	MAX_POP_SIZE = 200,
 	MUT_INTERVAL = 3,
 	REP_INTERVAL = 15,
 	D_INTERVAL = 30,
@@ -60,7 +59,7 @@ public class Simulator {
 
 
 	public static void main(String[] args) {
-		
+
 
 		//initialPopulation(initialPopulationSize);
 		//cityGenerator();
@@ -70,7 +69,30 @@ public class Simulator {
 			textMenu();
 			System.out.println();
 			inputMenu();
-		}while(run);
+			if(I_POP_SIZE != 20)
+			for(int i = 0; i < I_POP_SIZE; i++){
+				individuals(eventQueue, population);
+			}
+		}while(!run);
+			//time mode
+			if(SIM_MODE == 1){
+				System.out.println("Hej 1");
+			}
+			//Every n events
+			else if(SIM_MODE == 2){
+				System.out.println("Hej 2");
+			}
+			//Verbose mode
+			else if(SIM_MODE == 3){
+				for(int j = 0; j < I_POP_SIZE; j++){
+				System.out.println(eventQueue.next().type());
+			}
+				System.out.println(population.size());
+			}
+			//Silent mode
+			else if(SIM_MODE == 4){
+				System.out.println("Hej 3");
+			}
 	}
 
 	/*
@@ -105,60 +127,60 @@ public class Simulator {
 			System.out.println("Select the options here: ");
 			userInput = input.nextInt();
 			switch(userInput) {
-			   	case INITIAL_POPULATION_SIZE: 
+			   	case INITIAL_POPULATION_SIZE:
 			   		System.out.println("Population size has been chosen");
-			   		I_POP_SIZE = input.nextInt(); 
+			   		I_POP_SIZE = input.nextInt();
 			   		System.out.println("Population size is " + I_POP_SIZE);
 			   	break;
-				case MAXIMUM_POPULATION_SIZE: 
+				case MAXIMUM_POPULATION_SIZE:
 			   		System.out.println("Maximum population size has been chosen");
 					MAX_POP_SIZE = input.nextInt();
 					System.out.println("Maximum population size is " + MAX_POP_SIZE);
 				break;
-				case MUTATION_INTERVAL: 
+				case MUTATION_INTERVAL:
 			   		System.out.println("Mutation interval has been chosen");
-					MUT_INTERVAL = input.nextInt(); 
+					MUT_INTERVAL = input.nextInt();
 					System.out.println("Mutation interval is " + MUT_INTERVAL);
 					break;
-				case REPRODUCTION_INTERVAL: 
+				case REPRODUCTION_INTERVAL:
 			   		System.out.println("Reproduction interval has been chosen");
-					REP_INTERVAL = input.nextInt(); 
+					REP_INTERVAL = input.nextInt();
 					System.out.println("Reproduction interval " + REP_INTERVAL);
 					break;
-				case DEATH_INTERVAL: 
+				case DEATH_INTERVAL:
 			   		System.out.println("Death interval has been chosen");
-					D_INTERVAL = input.nextInt(); 
+					D_INTERVAL = input.nextInt();
 					System.out.println("Death interval is " + D_INTERVAL);
 					break;
-				case COMFORT_NORMALIZATION: 
+				case COMFORT_NORMALIZATION:
 			   		System.out.println("Comfort normalization has been chosen");
-					COM_NOMRAL = input.nextDouble(); 
+					COM_NOMRAL = input.nextDouble();
 					System.out.println("Comfort normalization is " + COM_NOMRAL);
 					break;
-				case TOTAL_SIMULATION_TIME: 
+				case TOTAL_SIMULATION_TIME:
 			   		System.out.println("Total simulation time has been chosen");
-					T_SIM_SIZE = input.nextDouble(); 
+					T_SIM_SIZE = input.nextDouble();
 					System.out.println("Total simulation time is " + T_SIM_SIZE);
 					break;
 				case SIMULATION_MODE:
 					System.out.print("Simulation mode (1: every t units; 2: every n events; 3: verbose; 4: silent): ");
 					SIM_MODE = input.nextInt();
 					System.out.println("Simulation mode is " + SIM_MODE);
-				case START_PROGRAM: 
+				case START_PROGRAM:
 					if(debug)
 			    		System.out.println("Starting program");
 					break;
-				case EXIT_PROGRAM: 
+				case EXIT_PROGRAM:
 					if(debug)
 						System.out.println("Exiting program");
 					System.exit(0);
-				default: 
+				default:
 					System.out.println("Invalid option. Please try again.");
 					System.out.println();
 				break;
 			}
 		}while(userInput != START_PROGRAM);
-	}		
+	}
 
 	/*
 	* Allows the user to interact with the program.
@@ -181,62 +203,11 @@ public class Simulator {
 
 	}
 
-	public static void test(){
-		/*
-		* Initializes EventQueue and Population instances.
-		* CityGenerator has a static method called generate()
-		* and it is therefore not neccesary to initialize CityGenerator as an instance.
-		*/
-		//EventQueue eventQueue = new EventQueue();
-		//Population population = new Population(0.003);
-		
-
-		/*
-		* Prints out the name of the cities (test).
-		*/
-
-		// for(int i = 0; i < CityGenerator.generate().length; i++){
-		// 	System.out.println(CityGenerator.generate()[i].name());
-		// }
-
-		//population.add(individual = new Individual());
-		//System.out.println(population.size());
-		/*switch(userInput) {
-	    	case INITIAL_POPULATION_SIZE: break();
-			case MAXIMUM_POPULATION_SIZE: break();
-			case MUTATION_INTERVAL: break();
-			case REPRODUCTION_INTERVAL: break();
-			case DEATH_INTERVAL: break();
-			case COMFORT_NORMALIZATION: break ();
-			case TOTAL_SIMULATION_TIME: break();
-			case START_PROGRAM: break();
-			case EXIT_PROGRAM: break ();
-
-	    	default: System.out.println("Invalid option. Please try again.");
-			System.out.println();
-			break;
-			}
-			*/
-	}
-	
-
-	private static void initialPopulation(int size){
-		int i = 0;
-		int j = 0;
-		while(i < size) {
-			population.add(individual = new Individual(CityGenerator.generate()));
-			for(j = 0; j < CityGenerator.generate().length; j++){
-			System.out.print(individual.path()[j].name());
-		}
-			System.out.println(population.size());
-			i++;
-		}
-	}
 	private static void maxPopulation(int population){
 	}
 
 	/*
-	* Prints the name and the coordinates of the cities. 
+	* Prints the name and the coordinates of the cities.
 	*/
 
 	private static void cityGenerator(){
@@ -258,11 +229,11 @@ public class Simulator {
 			if(i < (CityGenerator.generate().length-1))
 				System.out.print("; ");
 
-			/* 
+			/*
 			* The formula for the euclidean distance is being calculated here.
 			* The formula is square root of delta x raised to 2 and delta y raised to 2.
 			* The algoritm will stop, if the it does not find the next x and y in the arraylist.
-			*/ 
+			*/
 
 			if(i < (CityGenerator.generate().length-1)) {
 				euclideanDistance = Math.sqrt(Math.pow((population.bestPath()[i+1].x() - population.bestPath()[i].x()), 2) + Math.pow((population.bestPath()[i+1].y() - population.bestPath()[i].y()), 2));
@@ -274,7 +245,37 @@ public class Simulator {
 				System.out.println("The system is done calculating.");
 	}
 	System.out.print(" (cost: "+ total + ")");
-}
+	}
+	public static void individuals(EventQueue eventQueue,Population population){
+		//Creates a new individual and places him in population
+		City[] city = CityGenerator.generate();
+		Individual individual = new Individual(city);
+		population.add(individual);
+
+		//Defines the probability of events
+		final double fit = 1 - Math.log(population.fitness(individual));
+		final double mut = RandomUtils.getRandomTime(fit * MUT_INTERVAL);
+		final double rep = RandomUtils.getRandomTime(fit * I_POP_SIZE/MAX_POP_SIZE * REP_INTERVAL);
+		final double dead = RandomUtils.getRandomTime(fit * D_INTERVAL);
+
+		//Makes an object type event.
+		//Event(char type, double time, Individual individual) - To remember parameters
+		Event eventMutation = new Event('M', mut, individual);
+		Event eventReproduction = new Event('R', rep, individual);
+		Event eventDeath = new Event('D', dead, individual);
 
 
+		//adds events to eventQueue
+		eventQueue.add(eventMutation);
+		eventQueue.add(eventReproduction);
+		eventQueue.add(eventDeath);
+
+		System.out.println("Dette er mutation probability " + mut);
+		System.out.println(fit);
+		System.out.println(I_POP_SIZE);
+		System.out.println(REP_INTERVAL);
+		System.out.println(MAX_POP_SIZE);
+		System.out.println("Dette er Reproduction probability " + rep);
+		System.out.println("Dette er death probability " + dead + '\n');
+	}
 }
